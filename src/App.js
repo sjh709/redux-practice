8e2dda8f5aa19cbda22c27d2d5feb9e847dc0029
Jihee <sjh709@naver.com>
2024-01-15 14:57:48 +0900

    useSelector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import Box from './component/Box';
 
 function App() {
   const count = useSelector((state) => state.count);
@@ -11,6 +12,7 @@ function App() {
     <div>
       <h1>{count}</h1>
       <button onClick={increase}>증가!</button>
+      <Box />
     </div>
   );
 }
