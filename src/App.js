69656d05e0f8c79a63f30c11df1a883d4b0b1a95
Jihee <sjh709@naver.com>
2024-01-15 15:03:43 +0900

    payload

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import Box from './component/Box';
 
 function App() {
   const count = useSelector((state) => state.count);
+  let id = useSelector((state) => state.id);
+  let password = useSelector((state) => state.password);
   const dispatch = useDispatch();
   const increase = () => {
-    dispatch({ type: 'INCREMENT' });
+    dispatch({ type: 'INCREMENT', payload: { num: 5 } });
+  };
+  const login = () => {
+    dispatch({ type: 'LOGIN', payload: { id: 'aa', password: '123' } });
   };
   return (
     <div>
+      <h1>
+        {id},{password}
+      </h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가!</button>
+      <button onClick={login}>Login</button>
       <Box />
     </div>
   );
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,10 +1,19 @@
 let initialState = {
   count: 0,
+  id: '',
+  password: '',
 };
 function reducer(state = initialState, action) {
+  //   console.log('action', action);
   switch (action.type) {
     case 'INCREMENT':
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + action.payload.num };
+    case 'LOGIN':
+      return {
+        ...state,
+        id: action.payload.id,
+        password: action.payload.password,
+      };
     default:
       return { ...state };
   }
