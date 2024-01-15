79dd6615f302c6410682b363724741ed2b774d32
Jihee <sjh709@naver.com>
2024-01-15 14:51:17 +0900

    redux 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가!</button>
     </div>
   );
 }
